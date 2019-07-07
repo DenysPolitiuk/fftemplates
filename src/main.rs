2dc44ac798ba981253bec4bd8cd4ba0563674965
DenysPolitiuk <denys.politiuk@protonmail.com>
2019-07-07 18:38:27 -0400

    First commit

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// TODO:
+// 1) Take a base profile as an input
+// 2) Create a temporary folder for new profile
+// 3) Copy existing profile to temp folder
+// 4) Start firefox process with `firefox --profile <path>` where path is profile in temp folder
+// 5) Wait until firefox process is running
+// 6) Remove temporary profile and folder
+// 7) End wrapper's process
+fn main() {
+    println!("Hello, world!");
+}
